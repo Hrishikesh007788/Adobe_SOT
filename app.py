008ce17f291e7c7f6d0bc318f33da3ab1fcfeeb8
Hrishikesh Dutta <hrishikeshdutta2000@gmail.com>
2023-08-14 17:37:59 +0530

    another

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app.secret_key = 'your_secret_key'  # Set your secret key for flash messages
 app.config['UPLOAD_FOLDER'] = 'uploads'
 
 bootstrap = Bootstrap(app)
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = "API"
 
 
 query = ""  # Variable to store the user's question
